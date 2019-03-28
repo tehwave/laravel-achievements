fc32ca1a91d8d2b01f2c8da1e1b69f879dc1161a
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2019-03-28 20:53:24 +0100

    Create 2019_00_00_000000_create_achievements_table.php

---
+++ database/migrations/2019_00_00_000000_create_achievements_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAchievementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('achievements', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('type');
+            $table->morphs('achiever');
+            $table->text('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('achievements');
+    }
+}
