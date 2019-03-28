2af324ff297c420d9a3c0e770b9e0b04a7648ddf
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2019-03-28 22:35:57 +0100

    Update Achievement.php

--- src/Models/Achievement.php
+++ src/Models/Achievement.php
@@ -1,3 +1,46 @@
 <?php
 
-// TODO
+namespace tehwave\Achievements\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Achievement extends Model
+{
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = config('achievements.table', 'achievements');
+
+    /**
+     * The guarded attributes on the model.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'data' => 'array',
+    ];
+
+    /**
+     * Get the achiever entity that the achievement belongs to.
+     */
+    public function achiever()
+    {
+        return $this->morphTo();
+    }
+}
