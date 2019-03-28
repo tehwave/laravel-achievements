44427a08cfec89cc8926a5bceb354fb86780a309
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2019-03-28 22:36:00 +0100

    Update Achiever.php

--- src/Traits/Achiever.php
+++ src/Traits/Achiever.php
@@ -1,3 +1,16 @@
 <?php
 
-// TODO
+namespace tehwave\Achievements\Traits;
+
+trait Achiever
+{
+    /**
+     * Get the entity's notifications.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    public function achievements()
+    {
+        return $this->morphMany(Achievement::class, 'achiever');
+    }
+}
