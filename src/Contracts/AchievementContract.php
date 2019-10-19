e39e1a4d8bc989ff5b361efca8961225415ba216
Peter Jørgensen <peterchrjoergensen@gmail.com>
2019-10-19 23:07:19 +0200

    fix missing functions

--- src/Contracts/AchievementContract.php
+++ src/Contracts/AchievementContract.php
@@ -13,4 +13,32 @@ interface AchievementContract
      * @return void
      */
     public static function unlock($achiever, $achievement);
+
+    /**
+     * Get the achievement's name.
+     *
+     * @return string
+     */
+    public function getNameAttribute();
+
+    /**
+     * Get the achievement's description.
+     *
+     * @return string
+     */
+    public function getDescriptionAttribute();
+
+    /**
+     * Get the achievement's icon.
+     *
+     * @return string
+     */
+    public function getIconAttribute();
+
+    /**
+     * Get the achievement's icon as an asset.
+     *
+     * @return string
+     */
+    public function getIconAsAssetAttribute();
 }
