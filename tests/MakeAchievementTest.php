484d61c69a1e8d61f42a4e9d26f0973211fa9323
Peter Jørgensen <peterchrjoergensen@gmail.com>
2019-12-30 16:16:20 +0100

    Create MakeAchievementTest.php

---
+++ tests/MakeAchievementTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace tehwave\Achievements\Tests;
+
+class MakeAchievementTest extends TestCase
+{
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        if (file_exists($path = $this->app->path('Achievements') . '/HelloWorld.php')) {
+            unlink($path);
+        }
+    }
+
+    /**
+     * Test the console command.
+     *
+     * @return void
+     */
+    public function testCommandMakesFile()
+    {
+        $this->artisan('make:achievement', ['name' => 'HelloWorld'])->assertExitCode(0);
+
+        $this->assertFileExists($this->app->path('Achievements') . '/HelloWorld.php');
+    }
+}
