66acee8e9c87f1ff241d9c980a55c52715ec497f
Peter Jørgensen <peterchrjoergensen@gmail.com>
2020-05-24 22:19:02 +0200

    Update achievements.php

--- config/achievements.php
+++ config/achievements.php
@@ -1,8 +1,17 @@
 <?php
 
 return [
+
     /*
-     * The database table to retrieve and store achievements.
-     */
+    |--------------------------------------------------------------------------
+    | Achievements Database Table
+    |--------------------------------------------------------------------------
+    |
+    | You may specify the table used to retrieve and store achievements.
+    | Of course, a sensible default is provided for you, and you're
+    | completely free to change the table to suit your needs.
+    |
+    */
+
     'table' => 'achievements',
 ];
